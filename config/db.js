ef072f66e808a523eff2c97464cd0d03f11f2ca9
Vinicius Miranda <vinniimiranda@gmail.com>
2019-01-27 23:56:12 -0200

    Mudança de BD, anterior era muito instável

--- config/db.js
+++ config/db.js
@@ -2,10 +2,10 @@ const mysql = require('mysql2')
 
 
 const connection = mysql.createConnection({
-	"host": 'us-cdbr-iron-east-03.cleardb.net',
-	"user": 'bf4f3c2d8fa8e2',
-	"password": 'd10993e7',
-	"database": "heroku_777b21d9213d397"
+	"host": 'sql10.freemysqlhosting.net	',
+	"user": 'sql10276074',
+	"password": '9TKFgJVFqi',
+	"database": "sql10276074"
 });
 
 module.exports = connection
