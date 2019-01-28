8ff6d3b402e8144595fb6021e480b641d2c15b2a
Vinicius Miranda <vinniimiranda@gmail.com>
2019-01-27 23:58:09 -0200

    Mudança de BD, anterior era muito instável

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ const mysql = require('mysql2')
 
 
 const connection = mysql.createConnection({
-	"host": 'sql10.freemysqlhosting.net	',
+	"host": 'sql10.freemysqlhosting.net',
 	"user": 'sql10276074',
 	"password": '9TKFgJVFqi',
 	"database": "sql10276074"
