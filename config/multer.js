2e254666e33a46b23b29ab879eeae01634fb9572
Vinicius Miranda <vinniimiranda@gmail.com>
2019-02-15 01:48:24 -0200

    Anexos TEMP

--- config/multer.js
+++ config/multer.js
@@ -1,9 +1,9 @@
 const multer =require('multer')
 const path = require('path')
 const crypto = require('crypto')
-console.log(path.resolve(__dirname, '..',  'temp', 'upload'))
+console.log(path.resolve(__dirname, '..', '..', 'temp', 'upload'))
 module.exports = { 
-    dest: path.resolve(__dirname, '..',  'temp', 'upload'),
+    dest: path.resolve(__dirname, '..', '..',  'temp', 'upload'),
     storage: multer.diskStorage({
         destination: (req, file, callback)=> {
             callback(null, path.resolve(__dirname, '..', '..',  'temp', 'upload'))
