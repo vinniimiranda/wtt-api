ea0f368c91e33f51d055060ec1f7aa31fbe41888
Vinicius Miranda <vinniimiranda@gmail.com>
2019-03-07 20:40:59 -0300

    Fix

--- controllers/ControllerEmail.js
+++ controllers/ControllerEmail.js
@@ -1,18 +1,21 @@
 "use strict";
-const sgMail = require('@sendgrid/mail');
-sgMail.setApiKey("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");
+
 
 module.exports = {
     async main(html, subject) {
 
+        const sgMail = require('@sendgrid/mail');
+        sgMail.setApiKey("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");
         const msg = {
             to: "vinniimiranda@gmail.com, lpfernandes_@hotmail.com",
             from: 'admin@gdocs.com',
             subject: subject,
             html: html,
         };
-        
         sgMail.send(msg);
+
+
+
     }
 
 }
